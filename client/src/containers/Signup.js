dd47975658893b28c59d4a3db94ca5bfdd2b51f9
joeljdv <joeldiazjdv@gmail.com>
2021-08-03 00:45:09 -0400

    allow user to choose avatar as profile pic

--- client/src/containers/Signup.js
+++ client/src/containers/Signup.js
@@ -34,6 +34,11 @@ const Signup = (props) => {
         })
     }
 
+    const handleClick = (e) => {
+        console.log(e.target.src)
+        setImg(e.target.src)
+    }
+
     const errorList = errors.map(e => <li>{e}</li>)
 
     if(props.loggedIn){
@@ -70,6 +75,13 @@ const Signup = (props) => {
                     <br/>
                     <input type="submit"/>
                 </form>
+                <img onClick={handleClick} tabindex="1"  className="avatar"src="https://cdn4.iconfinder.com/data/icons/avatars-21/512/avatar-circle-human-female-black-7-512.png"/>
+                <img onClick={handleClick} tabindex="2" className="avatar"src="https://i0.wp.com/i.ya-webdesign.com/images/girl-avatar-png-19.png" />
+                <img onClick={handleClick} tabindex="3" className="avatar"src="https://www.maxpixel.net/static/photo/640/Icon-Female-Avatar-Female-Icon-Red-Icon-Avatar-6007530.png"/>
+                <br/>
+                <img onClick={handleClick} tabindex="4" className="avatar" src="https://icon-library.com/images/avatar-icon-images/avatar-icon-images-4.jpg"/>
+                <img onClick={handleClick} tabindex="5" className="avatar" src="https://freepngimg.com/download/facebook/62681-flat-icons-face-computer-design-avatar-icon.png"/>
+                <img onClick={handleClick} tabindex="6" className="avatar" src="https://cdn4.iconfinder.com/data/icons/avatars-21/512/avatar-circle-human-male-black-7-512.png"/>
                 {errors ? <div>{errorList}</div> : null}  
             </div>
            
